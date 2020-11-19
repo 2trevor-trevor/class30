f7e611cfa9d351bc37fe9c507f31aa2ddafd49fe
2trevor-trevor <72892948+2trevor-trevor@users.noreply.github.com>
2020-11-19 17:43:21 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,80 +1,81 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg, platform;
-var bird, slingShot;
 
-function preload() {
-  backgroundImg = loadImage("sprites/bg.png");
+var car,wall;
+
+
+var speed, weight; 
+
+
+function setup() 
+{
+  createCanvas(800, 400);
+
+
+	speed=random(10,100)
+	weight=random(400,1500)
+
+
+	car=createSprite(50, 200, 50,50);   
+
+	car.velocityX = speed;
+
+	car.shapeColor=color(255);
+
+
+  
+  	wall=createSprite(700,200, 60, height/2)
+	wall.shapeColor=color(80,80,80)
+	  
+	  textSize(18);
+	  fill("white")
 }
 
-function setup() {
-  var canvas = createCanvas(1200, 400);
-  engine = Engine.create();
-  world = engine.world;
-
-  ground = new Ground(600, height, 1200, 20);
-  platform = new Ground(150, 305, 300, 170);
-
-  box1 = new Box(700, 320, 70, 70);
-  box2 = new Box(920, 320, 70, 70);
-  pig1 = new Pig(810, 350);
-  log1 = new Log(810, 260, 300, PI / 2);
-
-  box3 = new Box(700, 240, 70, 70);
-  box4 = new Box(920, 240, 70, 70);
-  pig3 = new Pig(810, 220);
-
-  log3 = new Log(810, 180, 300, PI / 2);
-
-  box5 = new Box(810, 160, 70, 70);
-  log4 = new Log(760, 120, 150, PI / 7);
-  log5 = new Log(870, 120, 150, -PI / 7);
-
-  bird = new Bird(200, 50);
-
-  //log6 = new Log(230,180,80, PI/2);
-  slingshot = new SlingShot(bird.body, { x: 200, y: 50 });
-}
 
 function draw() {
-  background(backgroundImg);
-  Engine.update(engine);
-  //strokeWeight(4);
-  box1.display();
-  box2.display();
-  ground.display();
-  pig1.display();
-  log1.display();
+  background(0);
 
-  box3.display();
-  box4.display();
-  pig3.display();
-  log3.display();
+  if (keyDown("space"))
+  {
+	  car.x=50;
+	  car.y=200;
+	  speed=random(10,100)
+	  car.velocityX = speed;
 
-  box5.display();
-  log4.display();
-  log5.display();
-
-  bird.display();
-  platform.display();
-  //log6.display();
-  slingshot.display();
-}
-
-function mouseDragged() {
-  Matter.Body.setPosition(bird.body, { x: mouseX, y: mouseY });
-}
-
-function mouseReleased() {
-  slingshot.fly();
-}
-function keyPressed() {
-  if (keyCode === 32) {
-    slingshot.attach(bird.body);
   }
+  
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+	  car.velocityX=0;
+	  
+	  var deformation=0.5 * weight * speed* speed/22509;
+	  
+	if(deformation>180)
+	{
+		car.shapeColor=color("blue");
+	}
+
+	if(deformation<180 && deformation>100)
+	{
+		car.shapeColor=color("red");
+	}
+
+	if(deformation<100)
+	{
+		car.shapeColor=color("pink");
+	}
+	text("deformation:"+ Math.round(deformation),500,50)
+	
+  
+	text("speed:" + Math.round(speed) ,500,70)
+	
+  
+	text("weight:" + Math.round(weight),500,90)
+	
+  }  
+
+ 
+  drawSprites();
+ 
 }
+
+
